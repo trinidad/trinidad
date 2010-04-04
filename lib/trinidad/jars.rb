2445d8f6905418d1ae2c58534323ff3fcdd7f746
David Calavera <david.calavera@gmail.com>
2010-04-04 16:59:04 +0200

    using jruby-rack 0.9.7-SNAPSHOT to solve some bugs

--- lib/trinidad/jars.rb
+++ lib/trinidad/jars.rb
@@ -3,13 +3,13 @@ $:.unshift(TRINIDAD_LIBS) unless
   $:.include?(TRINIDAD_LIBS) || $:.include?(File.expand_path(TRINIDAD_LIBS))
 
 module Trinidad
-  require "servlet-api-2.5-6.1.14"
-  require "jsp-api-2.1"
-  require "jsp-2.1"
+  require 'servlet-api-2.5-6.1.14'
+  require 'jsp-api-2.1'
+  require 'jsp-2.1'
   require 'tomcat-core'
   require 'jetty-util-6.1.14'
 
-  require "jruby-rack-0.9.5"
+  require 'jruby-rack-0.9.7-SNAPSHOT'
 
   module Tomcat
     include_package 'org.apache.catalina'
@@ -25,7 +25,7 @@ module Trinidad
   end
 
   module Rack
-    include_package "org.jruby.rack"
-    include_package "org.jruby.rack.rails"
+    include_package 'org.jruby.rack'
+    include_package 'org.jruby.rack.rails'
   end
 end
