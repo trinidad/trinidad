dce2b747ef14d06cd13ba67f1457bd1f1acc5e8d
David Calavera <david.calavera@gmail.com>
2010-04-17 11:29:48 +0200

    fixtures refactoring

--- spec/fixtures/trinidad_foo_extension.rb
+++ spec/fixtures/trinidad_foo_extension.rb
@@ -1,1 +1,23 @@
-require File.dirname(__FILE__) + '/foo'
+module Trinidad
+  module Extensions
+    class FooWebAppExtension < WebAppExtension
+      def configure(tomcat, app_context)
+        @options
+      end
+    end
+
+    class FooServerExtension < ServerExtension
+      def configure(tomcat)
+        @options
+      end
+    end
+
+    class FooOptionsExtension < OptionsExtension
+      def configure(parser, default_options)
+        parser.on('--foo') do
+          default_options[:bar] = true
+        end
+      end
+    end
+  end
+end
