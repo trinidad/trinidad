b33f3e5e0712ea6b85d293ea02bc85e048d16e42
nkostelnik <nkostelnik@gmail.com>
2010-07-29 23:21:57 +0800

    now sets the rack environment variable

--- lib/trinidad/rackup_web_app.rb
+++ lib/trinidad/rackup_web_app.rb
@@ -6,6 +6,7 @@ module Trinidad
       if rackup_path = rackup
         rackup_path = File.join(rackup_path, 'config.ru') if File.directory?(rackup_path)
         add_parameter_unless_exist('rackup.path', rackup_path)
+        add_parameter_unless_exist('rack.env', environment.to_s)
       end
       @params
     end
