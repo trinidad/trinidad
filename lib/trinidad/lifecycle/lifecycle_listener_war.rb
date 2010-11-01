da541dd477c27d0e358fec4a9bc98e3a901a8fa7
David Calavera <david.calavera@gmail.com>
2010-11-01 17:45:46 +0100

    a new context configuration is not required

--- lib/trinidad/lifecycle/lifecycle_listener_war.rb
+++ lib/trinidad/lifecycle/lifecycle_listener_war.rb
@@ -4,21 +4,12 @@ module Trinidad
       def configure_defaults(context)
         super
         configure_class_loader(context)
-        clean_context_configuration(context)
       end
 
       def configure_class_loader(context)
         loader = Trinidad::Tomcat::WebappLoader.new(@webapp.class_loader)
         loader.container = context
         context.loader = loader
-      end
-
-      def clean_context_configuration(context)
-        config = context.find_lifecycle_listeners.select {|listener| listener.instance_of? Trinidad::Tomcat::ContextConfig }
-        config.each { |c| context.remove_lifecycle_listener(c) }
-
-        config = Trinidad::Tomcat::ContextConfig.new
-        context.add_lifecycle_listener config
       end
     end
   end
--- spec/trinidad/lifecycle/lifecycle_listener_war_spec.rb
+++ spec/trinidad/lifecycle/lifecycle_listener_war_spec.rb
@@ -10,14 +10,4 @@ describe Trinidad::Lifecycle::War do
     @listener.configure_class_loader(@context)
     @context.loader.should_not be_nil
   end
-
-  it "creates a new context configuration with the default web.xml" do
-    @listener.clean_context_configuration(@context)
-    config = @context.find_lifecycle_listeners.select do |listener|
-      listener.instance_of? Trinidad::Tomcat::ContextConfig
-    end
-
-    config.should have(1).listener
-    config[0].default_web_xml.should == Trinidad::Tomcat::Constants::DefaultWebXml
-  end
 end
