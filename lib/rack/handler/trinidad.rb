18333417728e3a21a0d02d83964489ed442b3935
Florian Gilcher <florian.gilcher@asquera.de>
2011-09-17 11:54:05 +0200

    Make handler opts handling more robust

--- lib/rack/handler/trinidad.rb
+++ lib/rack/handler/trinidad.rb
@@ -9,9 +9,13 @@ module Rack
     class Trinidad < Rack::Handler::Servlet
       def self.run(app, options={})
         opts = options.dup
+
+        # some libs use :Port, :port and :Host, :host, unify this
+        opts.each {|k,v| opts[k.to_s.downcase.to_sym] = v}
+
         opts[:app] = app
-        opts[:port] = 3000
-        opts[:address] = (options[:Host] || 'localhost')
+        opts[:port] = opts[:port] || 3000
+        opts[:address] = (options[:host] || 'localhost')
 
         context = org.jruby.rack.embed.Context.new('Trinidad')
         dispatcher = org.jruby.rack.embed.Dispatcher.new(context, self.new(app))
