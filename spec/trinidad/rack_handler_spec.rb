trinidad/trinidad

commit e387e9845b0f9d433fa8b87f8092b42dc10ed26f

Require Rack handler by default
No considerable cost, but much better
usability when using trinidad as a
library.

lib/trinidad.rb
@@ -6,7 +6,6 @@ require 'jruby-rack'
 gem 'trinidad_jars'
 
 require 'trinidad/core_ext'
-
 require 'trinidad/extensions'
 require 'trinidad/configuration'
 require 'trinidad/command_line_parser'
@@ -22,6 +21,7 @@ require 'trinidad/web_app'
 require 'trinidad/rails_web_app'
 require 'trinidad/rackup_web_app'
 require 'trinidad/war_web_app'
+require 'rack/handler/tinidad'
 
 module Trinidad
   VERSION = '1.2.3'

commit bf26bec8ae34610cce817ba3ef2d26a354233a55

do not require rack/handler/trinidad.rb from trinidad.rb and let Rack::Handler load it during `rackup -s trinidad` (users using trinidad as a library should require 'rack/handler/trinidad' explicitly ) it's much clearer since `require 'trinidad'` does not pollute a NS different from Trinidad and avoids issues such as #48
introduced in e387e9845b0f9d433fa8b87f8092b42dc10ed26f
and reverts 44f4c81f1e916f0c0fb4e8b2ffcef3171df160b1 as well

bin/trinidad
@@ -2,5 +2,5 @@
 
 require "trinidad"
 
-opts = Trinidad::CommandLineParser.parse(ARGV)
+Trinidad::CommandLineParser.parse(ARGV)
 Trinidad::Server.new(Trinidad.configuration).start

lib/rack/handler/trinidad.rb
@@ -1,3 +1,8 @@
+# NOTE: require 'rack/handler/trinidad' might get invoked 2 ways :
+# 1. Rack::Handler.try_require('rack/handler', 'trinidad') during rackup
+#    in this case trinidad.rb might not yet been loaded
+# 2. a user require (after trinidad.rb booted) - need to load rack first
+require 'trinidad'
 require 'rack/handler'
 require 'rack/handler/servlet'
 

lib/trinidad.rb
@@ -18,7 +18,6 @@ require 'trinidad/web_app'
 require 'trinidad/rails_web_app'
 require 'trinidad/rackup_web_app'
 require 'trinidad/war_web_app'
-require 'rack/handler/trinidad'
 
 module Trinidad
   VERSION = '1.3.3'

spec/trinidad/rack_handler_spec.rb
@@ -1,35 +1,48 @@
 require 'spec_helper'
 
-describe Rack::Handler::Trinidad do
-  it "registers the trinidad handler" do
-    Rack::Handler.get(:trinidad).should == described_class
+describe 'Rack::Handler::Trinidad' do
+  
+  it "is not loaded by default" do
+    require 'trinidad'
+    defined?(Rack::Handler::Trinidad).should be nil
   end
+  
+  describe 'Rack::Handler::Trinidad' do
+    
+    before(:all) { require "rack/handler/trinidad" }
 
-  it "turns the threads option into jruby min/max runtimes" do
-    opts = described_class.parse_options({:threads => '2:3'})
-    opts[:jruby_min_runtimes].should == 2
-    opts[:jruby_max_runtimes].should == 3
+    it "registers the trinidad handler" do
+      Rack::Handler.get(:trinidad).should == Rack::Handler::Trinidad
+    end
+
+    it "turns the threads option into jruby min/max runtimes" do
+      opts = Rack::Handler::Trinidad.parse_options({:threads => '2:3'})
+      opts[:jruby_min_runtimes].should == 2
+      opts[:jruby_max_runtimes].should == 3
+    end
+
+    it "uses localhost:3000 as default host:port" do
+      opts = Rack::Handler::Trinidad.parse_options
+      opts[:address].should == 'localhost'
+      opts[:port].should == 3000
+    end
+
+    it "accepts host:port or address:port as options" do
+      opts = Rack::Handler::Trinidad.parse_options({:host => 'foo', :port => 4000})
+      opts[:address].should == 'foo'
+      opts[:port].should == 4000
+
+      opts = Rack::Handler::Trinidad.parse_options({:address => 'bar', :port => 5000})
+      opts[:address].should == 'bar'
+      opts[:port].should == 5000
+    end
+
+    it "creates a servlet for the app" do
+      servlet = Rack::Handler::Trinidad.create_servlet(nil)
+      servlet.context.server_info.should == 'Trinidad'
+      servlet.dispatcher.should_not be nil
+    end
+
   end
-
-  it "uses localhost:3000 as default host:port" do
-    opts = described_class.parse_options
-    opts[:address].should == 'localhost'
-    opts[:port].should == 3000
-  end
-
-  it "accepts host:port or address:port as options" do
-    opts = described_class.parse_options({:host => 'foo', :port => 4000})
-    opts[:address].should == 'foo'
-    opts[:port].should == 4000
-
-    opts = described_class.parse_options({:address => 'bar', :port => 5000})
-    opts[:address].should == 'bar'
-    opts[:port].should == 5000
-  end
-
-  it "creates a servlet for the app" do
-    servlet = described_class.create_servlet(nil)
-    servlet.context.server_info.should == 'Trinidad'
-    servlet.dispatcher.should_not be_nil
-  end
+    
 end