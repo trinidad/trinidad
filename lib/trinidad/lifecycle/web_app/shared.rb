68ef72bc384d2db8da0e46de4e38e599fb3b379a
kares <self@kares.org>
2012-09-16 16:54:24 +0200

    a taste of WebApp::Lifecycle::Shared refactoring - add a logger (delegating to the standard context log) - set doc_base (we "undocumented" overrides in config) - #set_work_dir replaced with #adjust_context

--- lib/trinidad/lifecycle/web_app/shared.rb
+++ lib/trinidad/lifecycle/web_app/shared.rb
@@ -17,42 +17,44 @@ module Trinidad
           super
           configure(event.lifecycle)
         end
-
+        
         # Configure the web application before it's started.
         def configure(context)
-          set_work_dir(context)
+          adjust_context(context)
           remove_defaults(context)
           configure_logging(context)
         end
 
         protected
-
-        def configure_logging(context)
-          Trinidad::Logging.configure_web_app(web_app, context)
+        
+        def adjust_context(context)
+          context.doc_base = web_app.doc_base if web_app.doc_base
+          context.work_dir = web_app.work_dir if web_app.work_dir
         end
         
-        def set_work_dir(context)
-          if web_app.work_dir
-            context.work_dir = web_app.work_dir
-          end
+        def configure_logging(context)
+          Trinidad::Logging.configure_web_app(web_app, context)
         end
         
         private
 
         def remove_defaults(context)
-          context.remove_welcome_file('index.jsp')
           context.remove_welcome_file('index.htm')
           context.remove_welcome_file('index.html')
+          remove_jsp_support(context)
+        end
 
+        def remove_jsp_support(context)
+          context.remove_welcome_file('index.jsp')
           if jsp_wrapper = context.find_child('jsp')
             remove_servlet_mapping(context, 'jsp')
             context.remove_child(jsp_wrapper)
+          else
+            logger.warn "[#{web_app.context_path}] jsp servlet not found"
           end
-
           context.process_tlds = false
-          context.xml_validation = false
         end
-
+        
         # Remove all servlet mappings for given (servlet) name.
         def remove_servlet_mapping(context, name)
           find_servlet_mapping(context, name).each do
@@ -68,6 +70,11 @@ module Trinidad
             name_mapping << pattern if mapping_for == name
           end
           name_mapping
+        end
+        
+        def logger
+          @logger ||= Trinidad::Logging::LogFactory.
+            getLog('org.apache.catalina.core.StandardContext')
         end
         
       end
