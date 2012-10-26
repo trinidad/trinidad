bf8110cc4c7d3660a3cade3a1b067001355cceae
kares <self@kares.org>
2012-10-26 07:39:22 +0200

    visually order lifcycle method defs as they happen

--- lib/trinidad/lifecycle/base.rb
+++ lib/trinidad/lifecycle/base.rb
@@ -17,12 +17,10 @@ module Trinidad
           before_init(event)
         when events::AFTER_INIT_EVENT then
           after_init(event)
-        when events::CONFIGURE_START_EVENT then
-          configure_start(event)
-        when events::CONFIGURE_STOP_EVENT then
-          configure_stop(event)
         when events::BEFORE_START_EVENT then
           before_start(event)
+        when events::CONFIGURE_START_EVENT then
+          configure_start(event)
         when events::START_EVENT then
           start(event)
         when events::AFTER_START_EVENT then
@@ -31,6 +29,8 @@ module Trinidad
           before_stop(event)
         when events::STOP_EVENT then
           stop(event)
+        when events::CONFIGURE_STOP_EVENT then
+          configure_stop(event)
         when events::AFTER_STOP_EVENT then
           after_stop(event)
         when events::BEFORE_DESTROY_EVENT then
@@ -48,16 +48,15 @@ module Trinidad
       
       def before_init(event); end
       def after_init(event); end
-
-      def configure_start(event); end
-      def configure_stop(event); end
       
       def before_start(event); end
+      def configure_start(event); end
       def start(event); end
       def after_start(event); end
       
       def before_stop(event); end
       def stop(event); end
+      def configure_stop(event); end
       def after_stop(event); end
       
       def before_destroy(event); end
