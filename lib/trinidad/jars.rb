f620223268f156021f0945f511b91cc9e264ef4a
kares <self@kares.org>
2013-04-26 11:47:35 +0200

    update Tomcat to 7.0.39

--- lib/trinidad/jars.rb
+++ lib/trinidad/jars.rb
@@ -3,8 +3,8 @@ load File.expand_path('../../trinidad-libs/tomcat-core.jar', File.dirname(__FILE
 load File.expand_path('../../trinidad-libs/trinidad-rb.jar', File.dirname(__FILE__))
 
 module Trinidad
-  TRINIDAD_JARS_VERSION = '1.2.2'
-  TOMCAT_VERSION = '7.0.37' unless const_defined?(:TOMCAT_VERSION)
+  TRINIDAD_JARS_VERSION = '1.2.3'
+  TOMCAT_VERSION = '7.0.39' unless const_defined?(:TOMCAT_VERSION)
 
   module Tomcat
     include_package 'org.apache.catalina'
