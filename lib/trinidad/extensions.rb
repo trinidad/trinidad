b051692b851fe423c61391eccb1efa5857d7307f
kares <self@kares.org>
2014-03-10 08:37:38 +0100

    allow to disable (inherited) web-app extension by specifying `name: false`

--- lib/trinidad/extensions.rb
+++ lib/trinidad/extensions.rb
@@ -8,7 +8,7 @@ module Trinidad
         end
       end if extensions
     end
-    
+
     def self.configure_server_extensions(extensions, tomcat)
       extensions.each do |name, options|
         if extension = extension(name, 'ServerExtension', options)
@@ -22,20 +22,22 @@ module Trinidad
     end
 
     def self.configure_webapp_extensions(extensions, tomcat, context)
-      extensions.each do |name, options|
+      extensions.map do |name, options|
+        next if options == false # allow to explicitly disable
         if extension = extension(name, 'WebAppExtension', options)
           extension.tomcat = tomcat
           if extension.method(:configure).arity == 2
-            extension.configure(tomcat, context) # #deprecated old way
+            extension.configure(tomcat, context) # @deprecated old way
           else
             extension.configure(context)
           end
+          extension
         end
       end if extensions
     end
-    
+
     protected
-    
+
     def self.extension(name, type, options)
       class_name = Helpers.camelize(name) << type; clazz = nil
       if ( const_defined?(class_name) rescue nil )
@@ -57,63 +59,63 @@ module Trinidad
     end
 
     private
-    
+
     def self.tomcat_like?(tomcat)
       tomcat.respond_to?(:server) && tomcat.respond_to?(:start) && tomcat.respond_to?(:stop)
     end
-    
+
     def self.camelize(string)
-      Helpers.deprecate("Trinidad::Extensions.camelize use the camelize helper " << 
+      Helpers.deprecate("Trinidad::Extensions.camelize use the camelize helper " <<
                         "available in your Extension")
       Helpers.camelize(string)
     end
-    
+
     class Extension
       include Helpers; extend Helpers
-      
+
       attr_reader :options
-      
+
       def initialize(options = {})
         @options = options ? options.dup : {}
       end
-      
+
       private
-      
+
       # Hash#symbolize
       def symbolize(options, deep = false)
         Trinidad::Configuration.symbolize_options(options, deep)
       end
-      
+
     end
 
     class WebAppExtension < Extension
-      
+
       attr_accessor :tomcat
-      
+
       def configure(context)
         raise NotImplementedError, "#{self.class.name}#configure(context) not implemented"
       end
-      
+
     end
 
     class ServerExtension < Extension
-      
+
       def configure(tomcat)
         raise NotImplementedError, "#{self.class.name}#configure(tomcat) not implemented"
       end
-      
+
       # #deprecated override tomcat by returning it from #configure
       def override_tomcat?; false; end
-      
+
     end
 
     class OptionsExtension < Extension
-      
+
       def configure(parser, default_options)
         raise NotImplementedError, "#{self.class.name}#configure(parser, default_options) not implemented"
       end
-      
+
     end
-    
+
   end
 end
--- spec/trinidad/extensions_spec.rb
+++ spec/trinidad/extensions_spec.rb
@@ -4,89 +4,95 @@ describe Trinidad::Extensions do
 
   let(:tomcat) { org.apache.catalina.startup.Tomcat.new }
   let(:context) { Trinidad::Tomcat::StandardContext.new }
-  
+
+  before :all do
+    Trinidad::Extensions.module_eval do
+      def self.load_extension(name)
+        load "trinidad_#{name}_extension.rb"
+      end
+    end
+  end
+
+  after :all do
+    Trinidad::Extensions.module_eval do
+      def self.load_extension(name)
+        require "trinidad_#{name}_extension"
+      end
+    end
+  end
+
+  after do
+    [ :FooServerExtension, :FooWebAppExtension ].each do |const|
+      if Trinidad::Extensions.const_defined?(const)
+        Trinidad::Extensions.send :remove_const, const
+      end
+    end
+  end
+
   it "configures the server with new stuff" do
     options = { :bar => :bazz }
     def options.dup; self; end # hack dup
-    
+
     extensions = { :foo => options } # fixtures/trinidad_foo_extension.rb
-    lambda {
-      Trinidad::Extensions.configure_server_extensions(extensions, tomcat)
-    }.should_not raise_error
-    
-    options[:foo].should == 'foo_server_extension'
-    
-    lambda {
-      Trinidad::Extensions.const_get(:FooServerExtension)
-    }.should_not raise_error
+    Trinidad::Extensions.configure_server_extensions(extensions, tomcat)
+    expect( options[:foo] ).to eql 'foo_server_extension'
+    expect( Trinidad::Extensions.const_get(:FooServerExtension) ).to_not be nil
   end
 
   it "configures the webapp with new stuff" do
     extensions = { :foo => { :bar => :bazz } } # fixtures/trinidad_foo_extension.rb
-    lambda {
-      Trinidad::Extensions.configure_webapp_extensions(extensions, tomcat, context)
-    }.should_not raise_error
-    
-    context.getDocBase.should == 'foo_web_app_extension'
-    
-    lambda {
-      Trinidad::Extensions.const_get(:FooWebAppExtension)
-    }.should_not raise_error
+    Trinidad::Extensions.configure_webapp_extensions(extensions, tomcat, context)
+    expect( context.getDocBase ).to eql 'foo_web_app_extension'
+    expect( Trinidad::Extensions.const_get(:FooWebAppExtension) ).to_not be nil
   end
 
   it "configures the webapp with new stuff (backward compatible)" do
     extensions = { :foo_old => {} } # fixtures/trinidad_foo_old_extension.rb
-    lambda {
-      Trinidad::Extensions.configure_webapp_extensions(extensions, tomcat, context)
-    }.should_not raise_error
-    
-    context.getDocBase.should == 'foo_old_web_app_extension'
+    Trinidad::Extensions.configure_webapp_extensions(extensions, tomcat, context)
+    expect( context.getDocBase ).to eql 'foo_old_web_app_extension'
   end
-  
+
+  it "skips the webapp extension with false" do
+    extensions = { :foo => false }
+    Trinidad::Extensions.configure_webapp_extensions(extensions, tomcat, context)
+    expect( context.getDocBase ).to_not eql 'foo_web_app_extension'
+    expect( Trinidad::Extensions.const_defined?(:FooWebAppExtension) ).to be false
+  end
+
   it "configures with nil options" do
     extensions = { :foo => nil } # fixtures/trinidad_foo_extension.rb
-    lambda {
-      Trinidad::Extensions.configure_server_extensions(extensions, tomcat)
-    }.should_not raise_error
+    Trinidad::Extensions.configure_server_extensions(extensions, tomcat)
   end
-  
+
   it "adds options to the command line parser" do
     require 'optparse'
     options = {}
     parser = OptionParser.new
     extensions = { :foo => {} } # fixtures/trinidad_foo_extension.rb
-    lambda {
-      Trinidad::Extensions.configure_options_extensions(extensions, parser, options)
-    }.should_not raise_error
+    Trinidad::Extensions.configure_options_extensions(extensions, parser, options)
 
-    lambda {
-      parser.parse! ['--foo']
-      options[:foo].should be true
-    }.should_not raise_error
+    parser.parse! ['--foo']
+    options[:foo].should be true
   end
 
   it "allows to override the tomcat's instance" do
     extensions = { :override => {} } # fixtures/trinidad_override_extension.rb
 
     extended = Trinidad::Extensions.configure_server_extensions(extensions, tomcat)
-    extended.should_not equal(tomcat)
+    expect( extended ).to_not eql(tomcat)
   end
 
   it "ignores extensions that don't exist for that scope" do
     extensions = { :override => {} } # fixtures/trinidad_override_extension.rb
-    
-    lambda {
-      Trinidad::Extensions.configure_webapp_extensions(extensions, tomcat, nil)
-    }.should_not raise_error
+
+    Trinidad::Extensions.configure_webapp_extensions(extensions, tomcat, nil)
   end
 
   it "ignores extension but warns about it when it doesn't exist" do
     extensions = { :missing => {} }
 
     Trinidad::Helpers.expects(:warn)
-    lambda {
-      Trinidad::Extensions.configure_webapp_extensions(extensions, tomcat, nil)
-    }.should_not raise_error
+    Trinidad::Extensions.configure_webapp_extensions(extensions, tomcat, nil)
   end
-  
+
 end
