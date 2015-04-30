934f1265ff5485028f25e0ba8413367fa0a8fb6a
kares <self@kares.org>
2015-04-30 20:50:54 +0200

    update tomcat to 7.0.55

--- lib/trinidad/jars.rb
+++ lib/trinidad/jars.rb
@@ -4,7 +4,7 @@ load File.expand_path('../../trinidad-libs/trinidad-rb.jar', File.dirname(__FILE
 
 module Trinidad
   TRINIDAD_JARS_VERSION = '1.3.0'
-  TOMCAT_VERSION = '7.0.47' unless const_defined?(:TOMCAT_VERSION)
+  TOMCAT_VERSION = '7.0.55' unless const_defined?(:TOMCAT_VERSION)
 
   ( Tomcat = Java::RbTrinidad::Jerry ).module_eval do
     include_package 'org.apache.catalina'
